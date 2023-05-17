78606086d6a85ea984b28355cda6e543bcbfbf48
Andrei Constantinescu <andreiec112@gmail.com>
2023-05-17 15:19:41 +0300

    Styled navbar, added custom font, added images

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,28 @@
 import {
-    Box,
     Flex,
     Spacer,
+    Center,
+    Text,
+    Image,
+    HStack
   } from '@chakra-ui/react';
-
 
 function Navbar() {
     return (
-        <Flex bg='blue.500' w='100%' h='3rem'>
-            <Box p='1' bg='red'>
-                Box1
-            </Box>
+        <Flex bg='white' w='100%' h='7rem' px='5rem' color='black'>
+            <Center>
+                <Image src='logo_uiux.jpg' alt='logo uiux' maxW='5rem'/>
+                <Text ml='15px' mb='1px' fontWeight='bold' fontSize='3xl'>Parchează</Text>
+            </Center>
 
             <Spacer />
 
-            <Box p='1' bg='red'>
-                Box2
-            </Box>
+            <HStack spacing='3rem'>
+                <Text>Acasă</Text>
+                <Text>Despre</Text>
+                <Text>Rezervă</Text>
+                <Text>Contact</Text>
+            </HStack>
         </Flex>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.css'
+import { extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+    fonts: {
+        body: 'Nunito'
+    }
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App />
         </ChakraProvider>
     </React.StrictMode>,
